70779c88ffdcfa48522d75d8b1c4cf03b413f1ec
msadeedashraf <157824309+msadeedashraf@users.noreply.github.com>
2025-07-22 21:16:45 -0400

    .

---
+++ day02/hello-world2/my-script.js
@@ -0,0 +1,17 @@
+//alert("testing my JS file");
+
+
+function getDate()
+{
+document.getElementById('demo').innerHTML = new Date(); 
+}
+
+
+/*
+var firstName = "Sadeed";
+var lastName = "Ashraf";
+
+var fullName = firstName + ' '  + lastName;
+
+console.log(`My name is : ${fullName}`);
+*/
