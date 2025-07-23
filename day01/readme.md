a85e2eed4a4bfc5cfb288db2761a9e3fb9a70273
msadeedashraf <157824309+msadeedashraf@users.noreply.github.com>
2025-07-22 20:02:43 -0400

    .

---
+++ day01/readme.md
@@ -0,0 +1,3 @@
+- We did setup and executed the Javascript in the Browser Console.
+
+[Day-1](/assets/javascript_in_console.png)
---
+++ day02/node_example/hello_world.js
@@ -0,0 +1,32 @@
+console.log("Hello my Name is Sadeed");
+
+var num1 = 10;
+var num2 = 30;
+var result = num1 + num2;
+console.log(`The total of num1 and num2 is : ${result}`);
+
+
+var firstName = "Sadeed";
+var lastName = "Ashraf";
+
+var fullName = firstName + ' '  + lastName;
+
+console.log(`My name is : ${fullName}`);
+
+
+console.log(typeof(num1));
+console.log(typeof(num2));
+console.log(typeof(result));
+console.log(typeof(fullName));
+
+var currentDate = new Date();
+console.log(currentDate);
+console.log(typeof(currentDate));
+
+
+
+
+
+
+
+
